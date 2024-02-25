ba345527ad5ac946a87e54fe7be58d27777a7413
unknown <uyerandisr@gmail.com>
2024-02-25 04:09:13 -0500

    implementation of getWeather by code and id

--- src/app/Services/weather-service.service.ts
+++ src/app/Services/weather-service.service.ts
@@ -14,9 +14,35 @@ export default class WeatherServiceService
   {
       return this.http.get('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid='+ environment.apiKey);
   }
-
+    // coordinates
   public getWeatherByCoord(lat: number, lon: number)
   {
       return this.http.get('https://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid='+ environment.apiKey);
   }
+        // city
+  public getWeatherByCity(city: string)
+  {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + environment.apiKey);
+  }
+    // city - country 
+  public getWeatherByCityCountry(city: string, country: string)
+  {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&appid=' + environment.apiKey);
+  }
+    // city - country - state 
+  public getWeatherByCityCountryState(city: string, country: string, state: string)
+  {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + state + ',' + country + '&appid=' + environment.apiKey);
+  }
+      // city id 
+  public getWeatherByCityId(id: string)
+  {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?id=' + id + '&appid=' + environment.apiKey);
+  }
+    // zip - country
+  public getWeatherByZip(zip: string, country: string)
+  {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',' + country + '&appid=' + environment.apiKey);
+  }
+
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,15 @@ export class AppComponent implements OnInit
 
   title = 'guajiritos-weather';
   weatherInterface!: Weather;
+
   latitud: number = 0;
   longitud: number = 0;
+
+  city_name: string = '';
+  country_code: string = '';
+  state_code: string = '';
+  city_id: string = '';
+  zip_code: string = '';
 
 constructor(private weatherService: weatherService)
 {}
@@ -45,9 +52,30 @@ public getWeather(): Weather
   return this.weatherInterface;
 }
 
-onSubmit() 
+onCoordSubmit() 
 {
   this.getWeatherByCoord(this.latitud, this.longitud);  
+}
+
+onLocationByCitySubmit() 
+{
+    if((this.state_code == '') && (this.country_code == ''))
+    {
+      this.getWeatherByCity(this.city_name);
+    }
+    else if((this.state_code == '') && (this.country_code !== ''))
+    {
+      this.getWeatherByCityCountry(this.city_name, this.country_code);
+    }
+    else if ((this.state_code !== '') && (this.country_code !== ''))
+    {
+      this.getWeatherByCityCountryState(this.city_name, this.country_code, this.state_code);
+    }
+}
+
+onCityIdSubmit() 
+{
+  this.getWeatherByCityId(this.city_id);
 }
 
 public getWeatherByCoord(lat: number, lon: number): Weather
@@ -55,7 +83,7 @@ public getWeatherByCoord(lat: number, lon: number): Weather
   this.weatherService.getWeatherByCoord(lat, lon).subscribe({
     next: (response: any) => {
       this.weatherInterface = response;
-      console.log('coord by latitud and longitud = '+this.weatherInterface)
+      console.log('coord by latitud and longitud = '+this.weatherInterface.name)
     },
     error: (error: any) =>{
       console.log('error showing data weather ');
@@ -63,6 +91,82 @@ public getWeatherByCoord(lat: number, lon: number): Weather
   });
   return this.weatherInterface;
 }
+
+onZipSubmit() 
+{
+  this.getWeatherByZip(this.zip_code, this.country_code);  
+}
+
+public getWeatherByCity(city: string): Weather
+{
+  this.weatherService.getWeatherByCity(city).subscribe({
+    next: (response: any) =>{
+      this.weatherInterface = response;
+      console.log('city: ' + this.weatherInterface.name);
+    },
+    error: (error: any)=>{
+      console.log('error showing city');
+    },
+  });
+  return this.weatherInterface
+}
+
+public getWeatherByCityCountry(city: string, country: string): Weather
+{
+  this.weatherService.getWeatherByCityCountry(city, country).subscribe({
+  next: (response: any) =>{
+    this.weatherInterface = response;
+    console.log('city - country: ' + this.weatherInterface.name);
+  },
+  error: (error: any)=> {
+    console.log('error showing sity - country');
+  },
+});
+return this.weatherInterface;
+}
+
+public getWeatherByCityCountryState(city: string, country: string, state: string): Weather
+{
+  this.weatherService.getWeatherByCityCountryState(city, country, state).subscribe({
+  next: (response: any) =>{
+    this.weatherInterface = response;
+    console.log('city - country - state: ' + this.weatherInterface.name);
+  },
+  error: (error: any)=> {
+    console.log('error showing city - country - state');
+  },
+});
+return this.weatherInterface;
+}
+
+public getWeatherByCityId(id: string): Weather
+{
+  this.weatherService.getWeatherByCityId(id).subscribe({
+  next: (response: any) =>{
+    this.weatherInterface = response;
+    console.log('city id: ' + this.weatherInterface.name);
+  },
+  error: (error: any)=> {
+    console.log('error showing city id');
+  },
+});
+return this.weatherInterface;
+}
+
+public getWeatherByZip(zip: string, country: string): Weather
+{
+  this.weatherService.getWeatherByZip(zip, country).subscribe({
+  next: (response: any) =>{
+    this.weatherInterface = response;
+    console.log('zip - country: ' + this.weatherInterface.name);
+  },
+  error: (error: any)=> {
+    console.log('error showing zip - country');
+  },
+});
+return this.weatherInterface;
+}
+
 
 }
 
